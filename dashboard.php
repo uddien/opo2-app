0d9e1b872c9db6295c0d800af01786d3d53c5763
uddien <azzammataram@gmail.com>
2024-05-31 22:03:09 +0700

    cek eyyo

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,7 @@
     <div class="col-10">
       <div class="jumbotron p-3 m-0">
         <h1 class="display-5">Selamat Datang <?= $nama; ?></h1>
-        <p class="jumbotron-body">di Aplikasi WEB RPS</p>
+        <p class="jumbotron-body">di Aplikasi WEB RPS eyyo</p>
         <hr>
         <p class="jumbotron-footter">WEB RPS <?= date('Y'); ?></p>
       </div>
